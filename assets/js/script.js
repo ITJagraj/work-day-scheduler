caf3afa98d27ebef4778c54dee9210c70acbabd9
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-28 18:48:32 -0400

    loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var today = moment().format('MMMM DD YYYY -  hh:mm:ss a');
 $("#currentDay").text(today);
 
 //assign saveBtn click listener
-$(".saveBtn").click(function(){
+$(".saveBtn").click(function () {
     console.log(this);
     var test = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
 
-//set items in the local storage
-localStorage.setItem(time, test);
+    //set items in the local storage
+    localStorage.setItem(time, test);
 })
 
 //load any saved data from LocalStorage
@@ -24,6 +24,36 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+function hour() {
+    var currenthour = moment().hour();
+
+    //loop over time blocks
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log( blockHour, currentHour)
+
+        if(blockHour <currenthour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if(blockHour === currenthour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+
+})
+
+}
+hour();
 
 
 
